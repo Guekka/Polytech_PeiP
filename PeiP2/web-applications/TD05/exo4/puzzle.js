3b4d97b554b4523c4e9a1ebd616eeaf70977ce24
Guekka <39066502+Guekka@users.noreply.github.com>
2022-05-22 19:57:44 +0200

    [web-applications] TD5 done

--- PeiP2/web-applications/TD05/exo4/puzzle.js
+++ PeiP2/web-applications/TD05/exo4/puzzle.js
@@ -1,28 +1,53 @@
 
 window.onload = function () {
+	function to_letter(digit) {
+		console.assert(digit >= 1 && digit <= 26, "digit must be between 1 and 26");
+		return 'abcdefghijklmnopqrstuvwxyz'[digit - 1];
+	}
 
-	// pour distinguer les premiers clics
-	// des seconds clics
-	let first_click = true;
+	function get_imgs() {
+		return document.getElementById("puzzle").getElementsByTagName('img');
+	}
 
 	// pour stocker la première image cliquée
 	let first_image;
 
-	// si "not_finished" est vrai, alors
-	// il reste des images à permuter
-	let not_finished = true;
-
 	// traîte le clic sur une image
 	function click_on() {
+		if (first_image == null) {
+			first_image = this;
+			return;
+		} if (first_image == this) {
+			return;
+		}
+
+		// on swap les images
+		[this.name, first_image.name] = [first_image.name, this.name];
+		[this.src, first_image.src] = [first_image.src, this.src];
+
+		first_image = null;
+
+		if (is_finished()) {
+			document.getElementById("result").style.visibility = "visible";
+		}
 
 	}
 
 	// teste si le puzzle est terminé
 	function is_finished() {
-
+		let imgs = get_imgs();
+		for (i = 0; i < imgs.length; i++) {
+			if (imgs[i].name != to_letter(i + 1)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	// ici, il faut relier la fonction "clic_on" à l'évènement "onclick"
 	// sur toutes les images contenues dans l'élément d'id "puzzle"
-
+	let imgs = get_imgs();
+	for (let img of imgs) {
+		img.onclick = click_on;
+	}
 };
--- PeiP2/web-applications/TD05/exo9/memory.js
+++ PeiP2/web-applications/TD05/exo9/memory.js
@@ -1,41 +1,94 @@
-
-// le tableau qui contient le chemin
-// du fichier image pour chaque image
-var array = [];
-
-// si clicked[i] == true alors array[i] est visible
-var clicked = [];
-
-// pour décider si un clic est
-// un premier clic ou non
-var first_click = true;
-
-// l'indice de la première image cliquée
-var first_index = 0;
-
+// première image cliquée
+let first_image = null;
 // le nombre total de paires de clics
-var clicks_number = 0;
+let clicks_number = 0;
+// nombre de paires de clics réussis
+let good_clicks_number = 0;
+// en attente de timeout
+let waiting = false;
 
-// the nombre de paires de clics réussis
-// (les paires de clics qui ont découvert
-// des images identiques)
-var good_clicks_number = 0;
+class ImageState {
+    // Create new instances of the same class as static attributes
+    static Hidden = new ImageState("hidden");
+    static Revealed = new ImageState("revealed");
+    static Selected = new ImageState("selected");
 
-// affecte à l'attribut src des deux images d'indice i et j
-// le source de l'image "point d'interrogation"
-function hide(i, j) {
-	
+    constructor(name) {
+        this.name = name
+    }
 }
 
-// gère le clic sur l'image d'indice n
-function click_image(n) {
-	
+function is_ignored_click(img) {
+    if (waiting === true)
+        return true;
+    if (img === first_image)
+        return true;
+    if (img.name === img.getAttribute('src')) // relative path
+        return true; // already revealed
+    return false;
 }
 
-// rempli le tableau array avec la valeur de
-// l'attribut 'name' des images
-function init() {
-	
+function set_state(state, ...imgs) {
+    const question_image = "images/question-mark.png";
+
+    for (let img of imgs) {
+        if (state === ImageState.Hidden) {
+            img.src = question_image;
+            img.style.border = "";
+        } else if (state === ImageState.Revealed) {
+            img.src = img.name;
+            img.style.border = "";
+        } else if (state === ImageState.Selected) {
+            img.src = question_image;
+            img.style.border = "2px solid red";
+        }
+    }
 }
 
-window.onload = init;
+function second_click(image1, image2) {
+    clicks_number++;
+    // Dans tous les cas, on révèle les images
+    set_state(ImageState.Revealed, image1, image2);
+
+    if (image1.name === image2.name) {
+        good_clicks_number++;
+        first_image = null;
+    } else {
+        waiting = true;
+        setTimeout(() => {
+            set_state(ImageState.Hidden, image1, image2);
+            first_image = null;
+            waiting = false;
+        }, 1000);
+    }
+}
+
+function handle_victory() {
+    if (good_clicks_number === 8) {
+        let display = document.getElementById("result");
+        result.innerHTML = "Bravo, vous avez gagné !";
+        result.style.visibility = "visible";
+    }
+}
+
+// gère le clic sur l'image
+function click_image() {
+    if (is_ignored_click(this))
+        return;
+
+    if (first_image === null) {
+        first_image = this;
+        set_state(ImageState.Selected, this);
+        return;
+    }
+
+    second_click(first_image, this);
+    handle_victory();
+}
+
+window.onload = function () {
+    imgs = document.getElementById("grid").getElementsByTagName('img');
+    for (let img of imgs) {
+        img.onclick = click_image;
+    }
+};
