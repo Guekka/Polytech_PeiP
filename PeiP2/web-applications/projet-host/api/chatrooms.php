24b9836cccdb9d0cbf29e30dd934d12417538a79
Edgar B <epersonnal@keemail.me>
2022-05-27 19:52:55 +0200

    [web-applications] Remove unused code

--- PeiP2/web-applications/projet-host/api/chatrooms.php
+++ PeiP2/web-applications/projet-host/api/chatrooms.php
@@ -3,9 +3,6 @@ include "../utils.php";
 default_header();
 
 $post = json_decode(file_get_contents("php://input"), true);
-/*
-print_r($post);
-exit();*/
 
 $missing = check_args($post, "user_id");
 if (!empty($missing)) {
